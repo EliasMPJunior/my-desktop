86cba19e11ab069c8118fd0cdacb44c42a40b412
Elias Magalhães <eliasmpj@yahoo.com.br>
2026-08-05 18:24:02 -0300

    fix(labsea): load schedule relations before workstream board

--- techcenter-doc/.__ontobdc__/asset/infobim-view/js/pyodide_bootstrap.js
+++ techcenter-doc/.__ontobdc__/asset/infobim-view/js/pyodide_bootstrap.js
@@ -71,6 +71,7 @@
     loadStyle("../css/workstream_board.css");
     loadStyle("../css/workstream_schedule_summary.css");
     await loadLegacyWorkstreamDataWhenNeeded();
+    await loadScript("workstream_schedule_relations.js");
     await loadScript("workstream_board.js");
   }
 
